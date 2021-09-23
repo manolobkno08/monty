ad4f9495bd3c1008e3a0d9686486daa3bc9af212
afecego <afecego@gmail.com>
2021-09-22 20:31:53 -0500

    Funtions of project secod part

---
+++ monty_list_2.c
@@ -0,0 +1,82 @@
+#include "monty.h"
+
+/**
+ * func_pop - function to remove elements top of list
+ * @stack: double pointer to struct
+ * @line_number: number of lines into the file
+ * Return: Return print list
+ */
+void func_nop(stack_t **stack, unsigned int line_number)
+{
+	(void)(stack);
+	(void)(line_number);
+}
+
+/**
+ * func_swap - function
+ * @stack: double pointer to struct
+ * @line_number: number of lines into the file
+ * Return: Return print list
+ */
+void func_swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = NULL;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		printf("L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	tmp = (*stack)->next;
+	if (tmp->next != NULL)
+	{
+		(*stack)->next = tmp->next;
+		(*stack)->next->prev = *stack;
+
+	}
+	else
+	{
+		tmp->prev->prev = tmp;
+		tmp->prev->next = NULL;
+	}
+	tmp->prev = NULL;
+	tmp->next = *stack;
+	(*stack) = tmp;
+}
+
+/**
+ * close_f - function for free memory
+ * @status: argument from function on_exit
+ * @arg: argument from function on_exit
+ * Return: nothing
+ */
+
+void close_f(int status, void *arg)
+{
+	FILE *close = (FILE *)arg;
+
+	(void)(status);
+
+	fclose(close);
+}
+
+/**
+ * func_add - function to remove elements top of list
+ * @stack: double pointer to struct
+ * @line_number: number of lines into the file
+ * Return: Return print list
+ */
+void func_add(stack_t **stack, unsigned int line_number)
+{
+	int n = 0;
+
+	if (vars.stack_len < 2)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	n = (*stack)->n;
+	func_pop(stack, line_number);
+	(*stack)->n += n;
+}
