4fc9a9bdbbe68de0e8a35f69bc9b0076a0cf4892
afecego <afecego@gmail.com>
2021-09-22 20:25:13 -0500

    File match of funtions

---
+++ get_op_func.c
@@ -0,0 +1,82 @@
+#include "monty.h"
+
+/**
+ * get - matches command to appropriate function
+ * @token: var received to execute funcion
+ * @stack: structure of stack
+ * @line_number: number of lines into the file
+ * Return: result of function match
+ */
+
+void get(char *token, stack_t **stack, unsigned int line_number)
+{
+	instruction_t instr[] = {
+		{"push", func_push},
+		{"pall", func_pall},
+		{"pint", func_pint},
+		{"add", func_add},
+		{"pop", func_pop},
+		{"nop", func_nop},
+		{NULL, NULL}
+	};
+	int iter;
+
+	for (iter = 0; instr[iter].opcode != NULL; iter++)
+	{
+		if (strcmp(instr[iter].opcode, token) == 0)
+		{
+			instr[iter].f(stack, line_number);
+			return;
+		}
+	}
+	fprintf(stderr, "L%u: unknown instruction %s\n", line_number, token);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * free_stack - function for free memory
+ * @status: argument from function on_exit
+ * @arg: argument from function on_exit
+ * Return: nothing
+ */
+void free_stack(int status, void *arg)
+{
+	stack_t *next;
+	stack_t **stack;
+
+	(void)(status);
+
+	stack = (stack_t **)arg;
+    /* return if empty list */
+	if (*stack)
+	{
+		(*stack)->prev->next = NULL;
+		(*stack)->prev = NULL;
+	}
+
+	while (*stack && (*stack)->next)
+	{
+		next = (*stack)->next;
+		free(*stack);
+		*stack = next;
+	}
+	vars.stack_len = 0;
+}
+
+/**
+ * free_vars - function for free memory
+ * @status: argument from function on_exit
+ * @arg: argument from function on_exit
+ * Return: nothing
+ */
+
+void free_vars(int status, void *arg)
+{
+	char **buffer = arg;
+
+	(void)(status);
+	if (*buffer != NULL)
+	{
+		free(*buffer);
+	}
+}
